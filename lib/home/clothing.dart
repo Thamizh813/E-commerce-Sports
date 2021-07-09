2693496a68daa97cb8f899ecc98c5e8e737fd49e
Thamizh813 <75999346+Thamizh813@users.noreply.github.com>
2021-07-09 21:24:50 +0530

    cloth

--- lib/home/clothing.dart
+++ lib/home/clothing.dart
@@ -18,37 +18,37 @@ class _ClothingState extends State<Clothing> {
     },
     {
       'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBVrEWnhsaOWgOXD8gHulwMpWGATdW-pFvAA&usqp=CAU',
+          'https://th.bing.com/th/id/OIP.e4yWGWIbK8__idL4RAy81gHaHa?pid=ImgDet&rs=1',
       'itemname': 'namehere',
       'itemprice': '1,299.00'
     },
     {
       'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBVrEWnhsaOWgOXD8gHulwMpWGATdW-pFvAA&usqp=CAU',
+          'https://th.bing.com/th/id/OIP.qIc-wd0KfiFjHgmTMJWD1AHaHa?pid=ImgDet&rs=1',
       'itemname': 'namehere',
       'itemprice': '1,299.00'
     },
     {
       'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBVrEWnhsaOWgOXD8gHulwMpWGATdW-pFvAA&usqp=CAU',
+          'https://5.imimg.com/data5/YS/XX/MY-8683446/men-sports-dress-500x500.jpg',
       'itemname': 'namehere',
       'itemprice': '1,299.00'
     },
     {
       'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBVrEWnhsaOWgOXD8gHulwMpWGATdW-pFvAA&usqp=CAU',
+          'https://th.bing.com/th/id/OIP.AH2lQJ_kVHD01IQnzpG1dAHaHa?pid=ImgDet&rs=1',
       'itemname': 'namehere',
       'itemprice': '1,299.00'
     },
     {
       'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBVrEWnhsaOWgOXD8gHulwMpWGATdW-pFvAA&usqp=CAU',
+          'https://th.bing.com/th/id/OIP.ajKKKYhYv3VjCxujn7iCigHaHa?pid=ImgDet&rs=1',
       'itemname': 'namehere',
       'itemprice': '1,299.00'
     },
     {
       'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBVrEWnhsaOWgOXD8gHulwMpWGATdW-pFvAA&usqp=CAU',
+          'https://th.bing.com/th/id/OIP.omtK7Z-693ED73hBb2gBwgHaHa?pid=ImgDet&rs=1',
       'itemname': 'namehere',
       'itemprice': '1,299.00'
     }
